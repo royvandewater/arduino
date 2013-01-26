a7710506715e95236c12e4c5de3902ad52cc0e01
Roy van de Water <roy@integrumtech.com>
2013-01-26 10:01:20 -0700

    Pull cpp code into the header file for the LedArray library

--- libraries/LedArray/LedArray.h
+++ libraries/LedArray/LedArray.h
@@ -1,13 +1,34 @@
 #ifndef _LED_ARRAY_H
 #define _LED_ARRAY_H
 
+#include <Arduino.h>
+
+#define MAX_BRIGHTNESS 200 // Must be a multiple of FADE_AMOUNT
+#define MIN_BRIGHTNESS 0   // Must be a multiple of FADE_AMOUNT
+#define FADE_AMOUNT 5
+
 class LedArray {
 public:
-  LedArray();
+  LedArray(){
+    brightness     = 0;
+    fade_direction = 1;
+  };
 
-  void increment();
-  int getLed1Power();
-  int getLed2Power();
+  void increment(){
+    brightness = brightness + (FADE_AMOUNT * fade_direction);
+
+    if (brightness == MIN_BRIGHTNESS || brightness == MAX_BRIGHTNESS) {
+      fade_direction = -fade_direction;
+    }
+  };
+
+  int getLed1Power(){
+    return brightness;
+  }
+
+  int getLed2Power(){
+    return MAX_BRIGHTNESS - brightness;
+  }
 
 private:
   int brightness;
